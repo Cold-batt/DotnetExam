466ea3a93f290eee7646c84bff3db2f1ebff2207
Coldbatt <coldbatt1486@gmail.com>
2025-02-11 23:51:59 +0300

    Фиксы

---
+++ Backend/Itis.DotnetExam.Api.Contracts/Requests/User/GetRaiting/GetRatingResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace Itis.DotnetExam.Api.Contracts.Requests.User;
+
+/// <summary>
+/// Запрос на получение таблицы рейтинга
+/// </summary>
+public class GetRatingResponse
+{
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="items">Рейтинги</param>
+    public GetRatingResponse(List<GetRatingResponseItem> items)
+    {
+        Items = items;
+    }
+    
+    /// <summary>
+    /// Рейтинги
+    /// </summary>
+    public List<GetRatingResponseItem> Items { get; }
+}
